1eb589fa8a469567970511c2fac3fe5635bd2e9e
Marat Pereverzev <marat.pereverzrv@gmail.com>
2026-05-31 21:29:12 +0300

    Delete: removed dump generations Refactor: now you don't see triggering toast notifications of every single loaded tool, only the failed ones and warnings

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,6 +1,18 @@
 -- UI: statusline, dashboard, notifications, icons.
 -- Override LazyVim's UI plugins or add new ones here.
 return {
+    -- Only surface WARN/ERROR notifications; hide INFO chatter like
+    -- mason's "installing ..." messages. vim.notify is routed through
+    -- the snacks notifier in LazyVim.
+    {
+        "folke/snacks.nvim",
+        opts = {
+            notifier = {
+                level = vim.log.levels.WARN,
+            },
+        },
+    },
+
     -- Example: append a component to lualine.
     -- {
     --     "nvim-lualine/lualine.nvim",
